fcbbd4bd9659d72a21ee31453d1afb943a9f27a4
Admin <contact@hellowoot.co.kr>
2021-07-30 23:27:31 +0900

    add profile serializer and view logic

---
+++ app/api/account/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import User, Profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+
+    """
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'identifier'
+        ]
+
+        read_only_fields = fields
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+
+    """
+    class Meta:
+        model = Profile
+        fields = [
+            'name',
+            'devicetoken'
+        ]
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    유저와 프로필을 같이 가져오는 serializer
+    """
+    profile = ProfileSerializer()
+
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + [
+            'profile',
+        ]
---
+++ app/api/account/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets, status
+
+from app.api.account.serializers import ProfileSerializer
+from app.rest_utils.mixins import CustomResponseMixin
+
+from .models import Profile
+
+class ProfileAPIViewSet(CustomResponseMixin, viewsets.GenericViewSet):
+    """
+
+    """
+    queryset = Profile.objects.all()
+    # authentication_classes =
+    # permission_classes =
+    serializer_class = ProfileSerializer
+
+    def list(self, request):
+        return self.response(
+            status_code=status.HTTP_200_OK,
+            data=self.get_serializer(self.queryset, many=True).data
+        )
