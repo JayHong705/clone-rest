5ff3bba3fbfce75195e4eee2c530951b118b8aea
Admin <contact@hellowoot.co.kr>
2021-08-02 23:52:19 +0900

    change code style

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import SimpleRouter
-from app.api.account.views import ProfileAPIViewSet
+from app.api.account import views as profile_view
 
 # router.register(<prefix>, <viewset>, basename=<prefix>)
 router_v1 = SimpleRouter(trailing_slash=False)
-router_v1.register('profile', ProfileAPIViewSet, basename='profile')
+router_v1.register('profile', profile_view.ProfileAPIViewSet, basename='profile')
 
 urlpatterns = []
 
